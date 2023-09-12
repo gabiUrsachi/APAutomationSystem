3e32cab65aeafae6450e1a662b18a699c7b39e5c
Gabriela Ursachi <gabriela.ursachi@basware.com>
2023-09-12 09:43:33 +0300

    Error fix

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -2,6 +2,6 @@
 set -xe
 
   # Maven is used to build  and create a war file.
-  mvn -Dmaven.test.skip=true clean install -DDB_HOST=localhost
+  mvn -f ../../ -Dmaven.test.skip=true clean install -DDB_HOST=localhost
 
 
