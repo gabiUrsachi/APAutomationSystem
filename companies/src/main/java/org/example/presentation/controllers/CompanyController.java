3705472c349b81f9f4f93498f585c2bfb5fe48a0
Andrei Vavilov <andrei.vavilov@basware.com>
2023-09-13 15:49:51 +0300

    testo

--- companies/src/main/java/org/example/presentation/controllers/CompanyController.java
+++ companies/src/main/java/org/example/presentation/controllers/CompanyController.java
@@ -38,7 +38,8 @@ public class CompanyController {
     @GetMapping
     public List<CompanyDTO> getCompanies() {
 
-        return companyMapperService.mapToDTO(companyService.getCompanies());
+        return null;
+        //return companyMapperService.mapToDTO(companyService.getCompanies());
     }
 
 
