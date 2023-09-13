e7bcb6e95f2801657165761a38ab1a1cc3f15a6f
Andrei Vavilov <andrei.vavilov@basware.com>
2023-09-13 15:42:30 +0300

    pipeline fix

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,4 +3,4 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://ciaooooo-webappdeploymentbucket-1t0k3vvv4cqbx/core-0.0.1-SNAPSHOT.jar /home/ec2-user/core-0.0.1-SNAPSHOT.jar
+aws s3 cp s3://ciaooo-webappdeploymentbucket-awn2o50qo8du/core-0.0.1-SNAPSHOT.jar /home/ec2-user/core-0.0.1-SNAPSHOT.jar
