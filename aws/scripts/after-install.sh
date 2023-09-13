457ccaf24141dad0ff2594c485baea141f178233
Gabriela Ursachi <gabriela.ursachi@basware.com>
2023-09-13 16:05:55 +0300

    added spring service

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -5,6 +5,14 @@ set -xe
 # Copy war file from S3 bucket to tomcat webapp folder
 aws s3 cp s3://springappdeploy-webappdeploymentbucket-6e39ignkct61/core-0.0.1-SNAPSHOT.jar /home/ec2-user/app/core-0.0.1-SNAPSHOT.jar
 
+echo "
+Description=spring-app
 
-# Ensure the ownership permissions are correct.
-chown -R /home/ec2-user/app
+[Service]
+Type=simple
+User=ec2-user
+WorkingDirectory=/home/ec2-user
+ExecStart=/usr/bin/java -DDB_HOST=54.166.143.184 -jar /home/ec2-user/app/core-0.0.1-SNAPSHOT.jar
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/spring-app.service
--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+set -xe
+
+sudo systemctl start spring-app.service
