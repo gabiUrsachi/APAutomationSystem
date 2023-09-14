08d19dafe0e81e237621b8badf2b454aefb9a8b0
Andrei Vavilov <andrei.vavilov@basware.com>
2023-09-14 17:05:30 +0300

    updated the template

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,4 +3,4 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://ciaooooo-webappdeploymentbucket-1sa1te44xe529/core-0.0.1-SNAPSHOT.jar /home/ec2-user/core-0.0.1-SNAPSHOT.jar
+aws s3 cp s3://ciaoooo-webappdeploymentbucket-1g1hqvg6dqh7z/core-0.0.1-SNAPSHOT.jar /home/ec2-user/core-0.0.1-SNAPSHOT.jar
