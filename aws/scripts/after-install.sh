6675757dd5c93e3732974b6776997dc674cf6ef0
Andrei Vavilov <andrei.vavilov@basware.com>
2023-09-18 10:10:35 +0300

    pipe test

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,4 +3,4 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://ciaoooo-webappdeploymentbucket-1g1hqvg6dqh7z/core-0.0.1-SNAPSHOT.jar /home/ec2-user/core-0.0.1-SNAPSHOT.jar
+aws s3 cp s3://quai-de-metro-webappdeploymentbucket-19emufs0lu3d7/core-0.0.1-SNAPSHOT.jar /home/ec2-user/core-0.0.1-SNAPSHOT.jar
