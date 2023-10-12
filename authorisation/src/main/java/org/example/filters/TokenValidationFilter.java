c601ebf8a8166a36176064efe29c7be96591984d
Gabriela Ursachi <gabriela.ursachi@basware.com>
2023-10-12 11:01:28 +0300

    Deployment test

--- authorisation/src/main/java/org/example/filters/TokenValidationFilter.java
+++ authorisation/src/main/java/org/example/filters/TokenValidationFilter.java
@@ -27,7 +27,7 @@ public class TokenValidationFilter implements Filter {
 
     public TokenValidationFilter(AuthorisationControllerAdvice controllerAdvice) {
         this.controllerAdvice = controllerAdvice;
-        this.excludedUrls = List.of("/api/companies.*", "/api/users/login");
+        this.excludedUrls = List.of("/api/companies.*", "/api/users/login", "/api/files.*");
     }
 
     @Override
