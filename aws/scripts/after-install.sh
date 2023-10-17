e4c1416334ccc6d116025084d1e8c911a87c8dfe
Gabriela Ursachi <gabriela.ursachi@basware.com>
2023-10-17 09:49:16 +0300

    pipeline test for a new stack

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy file from S3 bucket to EC2 instance
-aws s3 cp s3:// backendstack-webappdeploymentbucket-vjm5mtf048ja/core-0.0.1-SNAPSHOT.jar	 /home/ec2-user/app/core-0.0.1-SNAPSHOT.jar
+aws s3 cp s3://backendstack-webappdeploymentbucket-a29vsjcppmuk/core-0.0.1-SNAPSHOT.jar	 /home/ec2-user/app/core-0.0.1-SNAPSHOT.jar
 
 
 stack_name="BackendStack"
