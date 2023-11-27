26336039e77b7fdaf97342d9f8254ede281d2374
Gabriela Ursachi <gabriela.ursachi@basware.com>
2023-11-27 15:50:07 +0200

    Trying again to run integration test

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -xe
 
-mvn clean install -DDB_HOST=localhost
+stack_name="BackendStack"
+db_private_ip=$(aws cloudformation describe-stacks --stack-name "$stack_name" --query "Stacks[0].Outputs[?OutputKey=='MongoEC2InstancePrivateIP'].OutputValue" --output text)
+
+mvn clean install -DDB_HOST="${db_private_ip}"
 
 
