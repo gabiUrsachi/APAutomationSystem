fb491c2a32eb534e93b971154a425a00efa0a42d
Gabriela Ursachi <gabriela.ursachi@basware.com>
2023-11-27 16:05:11 +0200

    Disabled again running of integration test

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -xe
 
-stack_name="BackendStack"
-db_private_ip=$(aws cloudformation describe-stacks --stack-name "$stack_name" --query "Stacks[0].Outputs[?OutputKey=='MongoEC2InstancePrivateIP'].OutputValue" --output text)
-
 mvn clean install -DDB_HOST="${db_private_ip}"
 
 
