e2f32b8d5a353662441d9230fc78f8fd32c49887
Gabriela Ursachi <gabriela.ursachi@basware.com>
2023-12-07 16:43:02 +0200

    Added some new plots

--- db_population/populate_db.ps1
+++ db_population/populate_db.ps1
@@ -9,7 +9,7 @@ Write-Host "generating collections"
 .\generators\mgodatagen.exe -f .\generators\invoice-w-o-proper-company-uuid.json -o .\unprocessed-collections\invoice-collection.json --prettyprint
 
 
-Write-Host "asigning company uuid for po and invoice transactions"
+Write-Host "assigning company uuid for po and invoice transactions"
 
 python utils\inject_uuid_correct_date.py .\unprocessed-collections\company-collection.json .\unprocessed-collections\po-collection.json purchaseOrder .\processed-collections\po-collection.json 2
 python utils\inject_uuid_correct_date.py .\unprocessed-collections\company-collection.json .\unprocessed-collections\invoice-collection.json invoice .\processed-collections\invoice-collection.json 3
